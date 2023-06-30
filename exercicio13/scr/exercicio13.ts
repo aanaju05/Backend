130d943848cff378db866e45fd50e93b29b20cd8
aanaju05 <130005729+aanaju05@users.noreply.github.com>
2023-06-29 21:46:59 -0300

    Add files via upload

---
+++ exercicio13/scr/exercicio13.ts
@@ -0,0 +1,34 @@
+
+let users = [
+	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+];
+
+type clients = {
+    cliente: string,
+    saldoTotal: number,
+    debitos: number[];
+}
+
+
+function getNegativeAmount(users: clients[]): clients[] {
+    calculateDebts();
+
+    return users.filter(clients => {
+      return clients.saldoTotal < 0;
+    })
+}
+
+function calculateDebts () {
+    return users.map(user => {
+        const sumDebts = user.debitos.reduce((acumulator: number, value: number) =>  value ? acumulator += value : 0 , 0);
+        user.saldoTotal -= sumDebts;
+        user.debitos = [];
+    });
+}
+
+console.log(getNegativeAmount(users));
